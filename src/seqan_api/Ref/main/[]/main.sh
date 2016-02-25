4c5cb01587fa37d4c69d59b02c9e8d6c5780ddf2
Yue Gan <gany.fml@gmail.com>
2016-02-25 15:51:26 -0600

    shabang added

--- src/seqan_api/Ref/main/[]/main.sh
+++ src/seqan_api/Ref/main/[]/main.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+#vim: set noexpandtab tabstop=2:
+
 set -v
 
 ref=AGTGTCAGTAGCATG
---
+++ src/seqan_api/Ref/main/location/main.cpp
@@ -0,0 +1,21 @@
+// vim: set noexpandtab tabstop=2:
+#include <seqan_api/Ref.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	//Create SeqIndex
+	SeqString ref_query(string(argv[1]));
+	SeqIndex ref_index(ref_query);
+
+	//Create Ref
+	SeqStringPtr ref_query_ptr = std::make_shared<SeqString>(ref_query);
+	Ref ref(ref_query_ptr);
+	ref.loadIndex(ref_index);
+
+	std::string query_seq = argv[2];
+	ref.find(query_seq);
+	cout << ref.location() << endl;
+}
