7a117c8877889552a2824daeb6a5cfa81e58b73b
Yue Gan <gany.fml@gmail.com>
2016-07-31 15:44:58 -0500

    WordHitsGroup.cpp: name changed, test case improved

--- src/spliced_aln/WordHitsGroup/main/locate_bridge_within_two_chunks_denovo/main.sh
+++ src/spliced_aln/WordHitsGroup/main/locate_bridge_within_two_chunks_denovo/main.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #vim: set noexpandtab tabstop=2:
 
+set -v 
+
 #        1**  **8
 # Ref   TTGTGGAGTT
 # Query TT......TT
@@ -10,6 +12,32 @@ query=TTTT
 ref=TTGTGGAGTT
 head_chunk_end_pos_in_ref_and_query=(1 1)
 tail_chunk_start_pos_in_ref_and_query=(8 2)
+num_backsearch=1
+
+./main.exe "$query" "$ref" "${head_chunk_end_pos_in_ref_and_query[@]}" "${tail_chunk_start_pos_in_ref_and_query[@]}" "$num_backsearch"
+
+#         2 **  **10
+# Ref   TTTAGTGGAGTTT
+# Query TTTA.....TTT
+#         2      4
+
+query=TTTATTT
+ref=TTTAGTGGAGTTT
+head_chunk_end_pos_in_ref_and_query=(2 2)
+tail_chunk_start_pos_in_ref_and_query=(10 4)
+num_backsearch=1
+
+./main.exe "$query" "$ref" "${head_chunk_end_pos_in_ref_and_query[@]}" "${tail_chunk_start_pos_in_ref_and_query[@]}" "$num_backsearch"
+
+#         2 **  **10
+# Ref   TTTGTGGAGATTT
+# Query TTT.....ATTT
+#         2      4
+
+query=TTTATTT
+ref=TTTGTGGAGATTT
+head_chunk_end_pos_in_ref_and_query=(2 2)
+tail_chunk_start_pos_in_ref_and_query=(10 4)
 num_backsearch=1
 
 ./main.exe "$query" "$ref" "${head_chunk_end_pos_in_ref_and_query[@]}" "${tail_chunk_start_pos_in_ref_and_query[@]}" "$num_backsearch"
